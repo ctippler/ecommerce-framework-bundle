97934dac54b2becfa7eb173cf55564dfab52dd2c
ctippler <christian.tippler@valantic.at>
2024-11-25 09:20:16 +0100

    Revert "Bugfix Pricing Rules -> https://github.com/pimcore/ecommerce-framework-bundle/pull/214"
    This reverts commit 8806162bb2d5735eb42693073372ab753691994d.

--- src/PricingManager/Rule/Dao.php
+++ src/PricingManager/Rule/Dao.php
@@ -66,19 +66,8 @@ class Dao extends AbstractDao
      */
     public function create(): void
     {
-        try {
-            $this->db->beginTransaction();
-            if (!$this->model->getId()) {
-                $this->create();
-            }
-
-            $this->update();
-            $this->db->commit();
-        } catch (\Exception $e) {
-            $this->db->rollBack();
-
-            throw $e;
-        }
+        $this->db->insert(self::TABLE_NAME, []);
+        $this->model->setId((int) $this->db->lastInsertId());
     }
 
     /**
